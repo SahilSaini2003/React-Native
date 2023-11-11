a2b3d0d7a44fbd782a02713a8772f098232b8dbe
Sahil <kingcodderno1@gmail.com>
2023-11-11 21:53:40 +0530

    FIlter Preview 60 percent completed

--- ExpenseManager/src/screens/historyScreen.js
+++ ExpenseManager/src/screens/historyScreen.js
@@ -1,32 +1,49 @@
-const { Text, View, StyleSheet, TouchableOpacity,Image } = require("react-native");
+const { Text, View, StyleSheet, TouchableOpacity, Image, SectionList } = require("react-native");
+// import { CheckBox } from '@react-native-community/checkbox';
+import CheckBox from '@react-native-community/checkbox';
 import { useState } from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 function HistoryScreen() {
   const [monthIsClicked, setMonthIsClicked] = useState(false);
-
+  const month = [{ data: ['All', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] }];
+  const length = month.length;
   return (
     // <Text>HistoryScreen</Text>
     <View style={styles.main}>
       <View style={styles.mainBox}>
         <View style={styles.filterBox}>
-          <View style={styles.filterButton}>
-            <TouchableOpacity style={{width: 100, height: 30, flexDirection: 'row'}} onPress={()=>{setMonthIsClicked(!monthIsClicked)}}>
-            <Text style={{ color: 'black', alignSelf: 'flex-start', fontSize: 20, marginLeft: 10}}>Month</Text>
-            {monthIsClicked ? (<Image source={require('../assets/images/down-arrow.png')} style={{width: 20, height:20, marginLeft: 6, marginTop: 6}} />) : (<Image source={require('../assets/images/up-arrow.png')} style={{width: 20, height:20, marginLeft: 6, marginTop: 6}} />)}
+          <View style={[styles.filterButton, { height: 364 }]}>
+            <TouchableOpacity style={{ width: 100, height: 30, flexDirection: 'row' }} onPress={() => { setMonthIsClicked(!monthIsClicked) }}>
+              <Text style={{ color: 'black', alignSelf: 'flex-start', fontSize: 20, marginLeft: 10 }}>Month</Text>
+              {monthIsClicked ? (<Image source={require('../assets/images/down-arrow.png')} style={{ width: 20, height: 20, marginLeft: 6, marginTop: 6 }} />) : (<Image source={require('../assets/images/up-arrow.png')} style={{ width: 20, height: 20, marginLeft: 6, marginTop: 6 }} />)}
             </TouchableOpacity>
-            
+            <View style={{ borderTopWidth: 2 }}>
+              <SectionList
+                sections={month}
+                renderItem={({ item }) => <View>
+                  <Text style={{ color: 'black', fontSize: 15, margin: 2, alignSelf: 'center' }}>{item}</Text>
+                                 </View>
+                }
+                keyExtractor={(item, index) => index}
+              />
+            </View>
+            <CheckBox
+                    disabled={false}
+                    value={monthIsClicked}
+                    // onValueChange={(newValue) => setToggleCheckBox(newValue)}
+                  /> 
           </View>
-          <View style={styles.filterButton}>
+          <View style={[styles.filterButton, { height: 30 }]}>
 
           </View>
-          <View style={styles.filterButton}>
+          <View style={[styles.filterButton, { height: 30 }]}>
 
           </View>
         </View>
         {/* <View style={styles.filterBreifBox}></View> */}
         <View style={styles.contentBox}>
-        <View></View>
+          <View></View>
         </View>
       </View>
     </View>
@@ -49,35 +66,37 @@ const styles = StyleSheet.create({
   },
   filterBox: {
     flexDirection: 'row',
-    alignContent: 'space-between',
-    alignItems: 'center',
+    // alignContent: 'space-between',
+    // alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute'
-
   },
   filterButton: {
     backgroundColor: '#FFF49C',
     width: 110,
-    height: 100,
+    // height: undefined,
     margin: 5,
     borderRadius: 10,
     borderWidth: 2,
-    justifyContent: 'space-between',
+    // justifyContent: 'space-between',
     // alignItems:'center',
-    flexDirection: 'row'
+    // flexDirection: 'row',
+    // display: 'none'#F3E0AC
+    zIndex: 1,
+    // position: 'absolute'
   },
-  filterBreifBox:{
+  filterBreifBox: {
     width: 15,
     height: 15,
     backgroundColor: 'blue',
-    position:'absolute',
-    right:10,
+    position: 'absolute',
+    right: 10,
     left: 100,
     top: 100
   },
   contentBox: {
     flex: 1,
-    borderTopWidth:2,
+    borderTopWidth: 2,
     marginTop: 45
     // backgroundColor: 'black'
   }
