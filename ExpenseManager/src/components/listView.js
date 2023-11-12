616ef4a56997ce6bf58ea23cd871ace8616edbff
Sahil <kingcodderno1@gmail.com>
2023-11-12 22:27:05 +0530

    Hanging by a side

--- ExpenseManager/src/components/listView.js
+++ ExpenseManager/src/components/listView.js
@@ -3,31 +3,37 @@ import { useState } from 'react';
 
 function listView(props) {
 
-    const [dataSelected = [], setDataSelected = []] = useState([false]);
-    const dataList = [];
-    const selectedDataArray = [];
-    const dataPush = (data) => {
-        dataList[data] = !dataList[data];
-        setDataSelected[data] = dataList[data];
-        if (dataList[data] == true) {
-            selectedDataArray.push(data);
-        } 
-        if (dataList[data] == false) {
-            selectedDataArray.pop(data);
-        } 
-        console.log(data);
-        console.log(dataList[data]);
-        console.log(dataSelected);
-        console.log(selectedDataArray);
-        // selectedMonth.push(data)
+    const [dataSelected, setDataSelected] = useState({});
+    const [dataArray, setDataArray] = useState([]);
+    const dataPush = async (data) => {
+
+        const updateDataSelected =  { ...dataSelected };
+        updateDataSelected[data] = !dataSelected[data];
+        setDataSelected(updateDataSelected);
+        if (updateDataSelected[data] == true) {
+            console.log('Push', data);
+            setDataArray(item => [...item, data]);
+        }
+        if (updateDataSelected[data] == false) {
+            console.log('pop', data);
+            setDataArray(item => {
+                item.filter(function(item) {
+                    return item != data;
+                })
+            })
+        }
+        // console.log(dataSelected);
+        console.log(dataArray);
+        console.log(updateDataSelected);
+        console.log(updateDataSelected[data]);
     }
 
     return (
         <View style={{ borderTopWidth: 2 }}>
             <SectionList
                 sections={props.data}
-                renderItem={({ item }) => <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between' }} onPress={()=>{dataPush(item)}} >
-                    <Text style={{ color: 'black', fontSize: 15, marginVertical: 2, marginHorizontal: 5, alignSelf: 'center' }}>{item}</Text>
+                renderItem={({ item }) => <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between' }} onPress={() => { dataPush(item) }} >
+                    <Text style={{ color: 'black', fontSize: 15, marginVertical: 2, marginHorizontal: 3, alignSelf: 'center' }}>{item}</Text>
                     {dataSelected[item] ? (<Image source={require('../assets/images/checkmark.png')} style={{ height: 15, width: 15, marginTop: 5, marginHorizontal: 5 }} />):
                     (<Image source={require('../assets/images/blank-check-box.png')} style={{ height: 15, width: 15, marginTop: 5, marginHorizontal: 5 }} />)}
                 </TouchableOpacity>
